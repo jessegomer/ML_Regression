8bb62089ace229416cb41a85d0a337496a89a6f0
Jesse Gomer <jessegomer@gmail.com>
2014-11-16 23:18:37 -0600

    Added in the basic benchmarks file
    This has the first two benchmarks, more will be added soon

---
+++ benchmark.py
@@ -0,0 +1,44 @@
+#jesse gomer, cs545 machine learning group
+import timeit
+import numpy as np
+from scipy import linalg
+from sklearn import datasets
+
+
+def time_function(f, *args):
+    results = []
+    for i in range(100):
+        start = timeit.default_timer()
+        f(*args)
+        end = timeit.default_timer()
+        results.append((end - start)*1000)
+    return {'median': np.median(results), 'best':min(results)}
+
+def time_xhat_y(rows, cols, informative):
+    data = datasets.make_regression(n_features=cols, n_samples=rows, n_informative=informative)
+    xhat = linalg.pinv2(data[0])
+    return time_function(lambda a,b: np.dot(a, b), xhat, data[1])
+
+def update(old, xhat, delta):
+    return old + xhat.dot(delta)
+
+def time_naive_update(rows, cols, informative, num_new):
+    data = datasets.make_regression(n_features=cols, n_samples=rows, n_informative=informative)
+    xhat = linalg.pinv2(data[0])
+    old = xhat.dot(data[1])
+    delta = np.concatenate((np.random.rand(num_new), np.zeros((rows - num_new))))
+    return time_function(update, old, xhat, delta)
+
+
+cols = 100
+informative = 100
+ratio_new = 0.01
+
+rows = 100
+
+print "number of columns:", cols
+while rows <= 1000000:
+    xhat_time = time_xhat_y(rows, cols, informative)
+    update_time = time_naive_update(rows, cols, informative, int(rows*ratio_new))
+    print "rows: {}\t\txhat*y: {}\t\tnaive update: {}".format(rows, xhat_time["best"], update_time["best"])
+    rows *= 10
